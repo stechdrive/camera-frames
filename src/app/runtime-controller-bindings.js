018891bbbc1124df49bdd93837d94b833c70f65a
stechdrive <214753235+stechdrive@users.noreply.github.com>
2026-04-04 22:38:32 +0900

    refactor: extract runtime controller bindings

---
+++ src/app/runtime-controller-bindings.js
@@ -0,0 +1,244 @@
+export function createRuntimeControllerBindings({
+	renderer,
+	scene,
+	store,
+	state,
+	viewportShell,
+	dropHint,
+	anchorDot,
+	assetController,
+	referenceImageController,
+	projectController,
+	historyController,
+	interactionController,
+	projectionController,
+	viewportProjectionController,
+	outputFrameController,
+	frameController,
+	viewportToolController,
+	measurementController,
+	viewportAxisGizmoController,
+	exportController,
+	updateDropHint,
+	updateUi,
+	updateOutputFrameOverlay,
+	setStatus,
+	startZoomToolDrag,
+	toggleMeasurementMode,
+	toggleZoomTool,
+	toggleViewportSelectMode,
+	toggleViewportReferenceImageEditMode,
+	toggleViewportTransformMode,
+	toggleViewportPivotEditMode,
+	openFiles,
+	clearSceneAssetSelection,
+	isInteractiveTextTarget,
+	executeViewportPieAction,
+	handleZoomToolDragMove,
+	handleZoomToolDragEnd,
+	handleResize,
+	fpsMovement,
+	pointerControls,
+	getActiveCamera,
+	guideOverlay,
+	syncGuideOverlayState,
+	syncViewportProjection,
+	syncShotProjection,
+	applyCameraViewProjection,
+	updateShotCameraHelpers,
+	getActiveCameraViewCamera,
+	getActiveViewportCamera,
+	updateCameraSummary,
+	t,
+	formatNumber,
+	frameAllCameras,
+	placeAllCamerasAtHome,
+	syncControlsToMode,
+	setExportStatus,
+	projectPresentationSyncSuspendedRef,
+} = {}) {
+	return {
+		renderer,
+		scene,
+		store,
+		state,
+		viewportShell,
+		dropHint,
+		anchorDot,
+		assetController,
+		importReferenceImageFiles:
+			referenceImageController.importReferenceImageFiles,
+		supportsReferenceImageFile:
+			referenceImageController.supportsReferenceImageFile,
+		updateDropHint,
+		updateUi,
+		updateOutputFrameOverlay,
+		setStatus,
+		startOrbitAroundHitDrag: (...args) =>
+			interactionController?.startOrbitAroundHitDrag(...args) ?? false,
+		startZoomToolDrag,
+		startLensAdjustDrag: (...args) =>
+			interactionController?.startLensAdjustDrag(...args) ?? false,
+		startShotCameraRollDrag: (...args) =>
+			interactionController?.startShotCameraRollDrag(...args) ?? false,
+		startViewportOrthographicPanDrag: (...args) =>
+			interactionController?.startViewportOrthographicPanDrag?.(...args) ??
+			false,
+		toggleMeasurementMode,
+		toggleZoomTool,
+		toggleViewportSelectMode,
+		toggleViewportReferenceImageEditMode,
+		toggleViewportTransformMode,
+		toggleViewportPivotEditMode,
+		openFiles,
+		startNewProject: () => projectController?.startNewProject(),
+		isProjectDirty: () => projectController?.isProjectDirty?.() ?? false,
+		isPackageDirty: () => projectController?.isPackageDirty?.() ?? true,
+		shouldWarnBeforeUnload: () =>
+			projectController?.shouldWarnBeforeUnload?.() ?? false,
+		syncProjectPresentation: () =>
+			projectController?.syncProjectPresentation?.(),
+		suspendProjectPresentationSync: (nextSuspended) => {
+			projectPresentationSyncSuspendedRef.value = Boolean(nextSuspended);
+		},
+		establishProjectDirtyBaseline: () =>
+			projectController?.establishProjectDirtyBaseline?.(),
+		saveProject: () => projectController?.saveProject(),
+		exportProject: () => projectController?.exportProject(),
+		undoHistory: () => historyController?.undoHistory(),
+		redoHistory: () => historyController?.redoHistory(),
+		clearSceneAssetSelection,
+		beginHistoryTransaction: (label) =>
+			historyController?.beginHistoryTransaction(label),
+		commitHistoryTransaction: (label) =>
+			historyController?.commitHistoryTransaction(label),
+		isInteractiveTextTarget,
+		isZoomInteractionMode: () =>
+			interactionController?.isZoomInteractionMode() ?? false,
+		isPieInteractionMode: () =>
+			interactionController?.isPieInteractionMode() ?? false,
+		isLensInteractionMode: () =>
+			interactionController?.isLensInteractionMode() ?? false,
+		isRollInteractionMode: () =>
+			interactionController?.isRollInteractionMode() ?? false,
+		isViewportOrthographicActive: () =>
+			interactionController?.isViewportOrthographicActive?.() ?? false,
+		applyNavigateInteractionMode: () =>
+			interactionController?.applyNavigateInteractionMode(),
+		ensurePerspectiveForViewportRotation: () =>
+			interactionController?.ensurePerspectiveForViewportRotation?.() ?? false,
+		captureViewportProjectionState: () =>
+			viewportProjectionController?.captureViewportProjectionState?.() ?? null,
+		restoreViewportProjectionState: (snapshot) =>
+			viewportProjectionController?.restoreViewportProjectionState?.(
+				snapshot,
+			) ?? false,
+		openViewportPieMenu: (...args) =>
+			interactionController?.openViewportPieMenu(...args) ?? false,
+		openViewportPieMenuAtCenter: (...args) =>
+			interactionController?.openViewportPieMenuAtCenter(...args) ?? false,
+		updateViewportPiePointer: (...args) =>
+			interactionController?.updateViewportPiePointer(...args),
+		finishViewportPieMenu: (...args) =>
+			interactionController?.finishViewportPieMenu(...args) ?? null,
+		closeViewportPieMenu: (...args) =>
+			interactionController?.closeViewportPieMenu(...args),
+		handleViewportPieAction: executeViewportPieAction,
+		isFrameSelectionActive: frameController.isFrameSelectionActive,
+		isReferenceImageSelectionActive: () =>
+			referenceImageController?.isReferenceImageSelectionActive?.() ?? false,
+		clearFrameSelection: frameController.clearFrameSelection,
+		clearReferenceImageSelection: () =>
+			referenceImageController?.clearReferenceImageSelection?.(),
+		clearOutputFrameSelection: outputFrameController.clearOutputFrameSelection,
+		handleOrbitAroundHitDragMove: (...args) =>
+			interactionController?.handleOrbitAroundHitDragMove(...args),
+		handleOrbitAroundHitDragEnd: (...args) =>
+			interactionController?.handleOrbitAroundHitDragEnd(...args),
+		handleZoomToolDragMove,
+		handleZoomToolDragEnd,
+		handleLensAdjustDragMove: (...args) =>
+			interactionController?.handleLensAdjustDragMove(...args),
+		handleLensAdjustDragEnd: (...args) =>
+			interactionController?.handleLensAdjustDragEnd(...args),
+		handleShotCameraRollDragMove: (...args) =>
+			interactionController?.handleShotCameraRollDragMove(...args),
+		handleShotCameraRollDragEnd: (...args) =>
+			interactionController?.handleShotCameraRollDragEnd(...args),
+		handleViewportOrthographicPanMove: (...args) =>
+			interactionController?.handleViewportOrthographicPanMove?.(...args),
+		handleViewportOrthographicPanEnd: (...args) =>
+			interactionController?.handleViewportOrthographicPanEnd?.(...args),
+		handleViewportOrthographicWheel: (...args) =>
+			interactionController?.handleViewportOrthographicWheel?.(...args) ??
+			false,
+		handleOutputFramePanMove: outputFrameController.handleOutputFramePanMove,
+		handleOutputFramePanEnd: outputFrameController.handleOutputFramePanEnd,
+		handleOutputFrameResizeMove:
+			outputFrameController.handleOutputFrameResizeMove,
+		handleOutputFrameResizeEnd:
+			outputFrameController.handleOutputFrameResizeEnd,
+		handleOutputFrameAnchorDragMove:
+			outputFrameController.handleOutputFrameAnchorDragMove,
+		handleOutputFrameAnchorDragEnd:
+			outputFrameController.handleOutputFrameAnchorDragEnd,
+		handleFrameDragMove: frameController.handleFrameDragMove,
+		handleFrameDragEnd: frameController.handleFrameDragEnd,
+		handleFrameResizeMove: frameController.handleFrameResizeMove,
+		handleFrameResizeEnd: frameController.handleFrameResizeEnd,
+		handleFrameRotateMove: frameController.handleFrameRotateMove,
+		handleFrameRotateEnd: frameController.handleFrameRotateEnd,
+		handleFrameAnchorDragMove: frameController.handleFrameAnchorDragMove,
+		handleFrameAnchorDragEnd: frameController.handleFrameAnchorDragEnd,
+		handleViewportTransformDragMove:
+			viewportToolController.handleViewportTransformDragMove,
+		handleViewportTransformDragEnd:
+			viewportToolController.handleViewportTransformDragEnd,
+		pickViewportAssetAtPointer:
+			viewportToolController.pickViewportAssetAtPointer,
+		handleMeasurementPointerDown: (...args) =>
+			measurementController?.handleMeasurementPointerDown?.(...args) ?? false,
+		handleMeasurementHoverMove: (...args) =>
+			measurementController?.handleMeasurementHoverMove?.(...args),
+		clearSelectedMeasurementPoint: () =>
+			measurementController?.clearSelectedMeasurementPoint?.() ?? false,
+		deleteSelectedMeasurement: () =>
+			measurementController?.deleteSelectedMeasurement?.() ?? false,
+		syncMeasurementSceneHelpers: () =>
+			measurementController?.syncMeasurementSceneHelpers?.(),
+		startOutputFrameAnchorDrag:
+			outputFrameController.startOutputFrameAnchorDrag,
+		syncMeasurementOverlay: () =>
+			measurementController?.syncMeasurementOverlay?.(),
+		syncViewportTransformGizmo:
+			viewportToolController.syncViewportTransformGizmo,
+		syncViewportAxisGizmo: () =>
+			viewportAxisGizmoController?.syncViewportAxisGizmo?.(),
+		exportController,
+		handleResize,
+		fpsMovement,
+		pointerControls,
+		getActiveCamera,
+		guideOverlay,
+		syncGuideOverlayState,
+		syncViewportProjection,
+		syncShotProjection,
+		applyCameraViewProjection,
+		updateShotCameraHelpers,
+		getActiveCameraViewCamera,
+		getActiveViewportCamera,
+		updateCameraSummary,
+		t,
+		formatNumber,
+		frameAllCameras,
+		placeAllCamerasAtHome,
+		syncControlsToMode,
+		applyInitialNavigateInteractionMode: () =>
+			interactionController?.applyNavigateInteractionMode({ silent: true }),
+		loadStartupUrls: () => assetController.loadStartupUrls(),
+		setExportStatus,
+		getShotCameraRollLock: () => store.shotCamera.rollLock.value,
+		setShotCameraRollAngleDegrees: (nextValue) =>
+			projectionController?.setShotCameraRollAngleDegrees?.(nextValue),
+	};
+}
---
+++ test/camera-frames-runtime-controller-bindings.test.ts
@@ -0,0 +1,208 @@
+import assert from "node:assert/strict";
+import { createRuntimeControllerBindings } from "../src/app/runtime-controller-bindings.js";
+
+{
+	let suspended = false;
+	const calls = [];
+	const bindings = createRuntimeControllerBindings({
+		renderer: { id: "renderer" },
+		scene: { id: "scene" },
+		store: { shotCamera: { rollLock: { value: true } } },
+		state: { mode: "camera" },
+		viewportShell: { id: "viewport-shell" },
+		dropHint: { id: "drop-hint" },
+		anchorDot: { id: "anchor-dot" },
+		assetController: {
+			loadStartupUrls: () => "loaded",
+		},
+		referenceImageController: {
+			importReferenceImageFiles: (...args) => args.join("|"),
+			supportsReferenceImageFile: (name) => name.endsWith(".png"),
+			isReferenceImageSelectionActive: () => true,
+			clearReferenceImageSelection: () => calls.push("clear-reference"),
+		},
+		projectController: {
+			startNewProject: () => calls.push("new"),
+			isProjectDirty: () => true,
+			isPackageDirty: () => false,
+			shouldWarnBeforeUnload: () => true,
+			syncProjectPresentation: () => calls.push("sync-project"),
+			establishProjectDirtyBaseline: () => calls.push("baseline"),
+			saveProject: () => calls.push("save"),
+			exportProject: () => calls.push("export"),
+		},
+		historyController: {
+			undoHistory: () => calls.push("undo"),
+			redoHistory: () => calls.push("redo"),
+			beginHistoryTransaction: (label) => `begin:${label}`,
+			commitHistoryTransaction: (label) => `commit:${label}`,
+		},
+		interactionController: {
+			startOrbitAroundHitDrag: () => true,
+			isZoomInteractionMode: () => true,
+			isPieInteractionMode: () => false,
+			isLensInteractionMode: () => true,
+			isRollInteractionMode: () => false,
+			isViewportOrthographicActive: () => true,
+			applyNavigateInteractionMode: (options) => options ?? "nav",
+			ensurePerspectiveForViewportRotation: () => true,
+			openViewportPieMenu: () => true,
+			openViewportPieMenuAtCenter: () => true,
+			updateViewportPiePointer: (...args) =>
+				calls.push(["pie-pointer", ...args]),
+			finishViewportPieMenu: () => "finish",
+			closeViewportPieMenu: () => calls.push("close-pie"),
+			handleOrbitAroundHitDragMove: (...args) =>
+				calls.push(["orbit-move", ...args]),
+			handleOrbitAroundHitDragEnd: (...args) =>
+				calls.push(["orbit-end", ...args]),
+			handleLensAdjustDragMove: (...args) => calls.push(["lens-move", ...args]),
+			handleLensAdjustDragEnd: (...args) => calls.push(["lens-end", ...args]),
+			handleShotCameraRollDragMove: (...args) =>
+				calls.push(["roll-move", ...args]),
+			handleShotCameraRollDragEnd: (...args) =>
+				calls.push(["roll-end", ...args]),
+			handleViewportOrthographicPanMove: (...args) =>
+				calls.push(["ortho-pan-move", ...args]),
+			handleViewportOrthographicPanEnd: (...args) =>
+				calls.push(["ortho-pan-end", ...args]),
+			handleViewportOrthographicWheel: () => true,
+		},
+		projectionController: {
+			setShotCameraRollAngleDegrees: (value) => calls.push(["roll-set", value]),
+		},
+		viewportProjectionController: {
+			captureViewportProjectionState: () => ({ mode: "ortho" }),
+			restoreViewportProjectionState: (snapshot) => snapshot.mode === "ortho",
+		},
+		outputFrameController: {
+			clearOutputFrameSelection: () => calls.push("clear-output"),
+			handleOutputFramePanMove: (...args) => calls.push(["pan-move", ...args]),
+			handleOutputFramePanEnd: (...args) => calls.push(["pan-end", ...args]),
+			handleOutputFrameResizeMove: (...args) =>
+				calls.push(["resize-move", ...args]),
+			handleOutputFrameResizeEnd: (...args) =>
+				calls.push(["resize-end", ...args]),
+			handleOutputFrameAnchorDragMove: (...args) =>
+				calls.push(["anchor-move", ...args]),
+			handleOutputFrameAnchorDragEnd: (...args) =>
+				calls.push(["anchor-end", ...args]),
+			startOutputFrameAnchorDrag: () => calls.push("start-anchor"),
+		},
+		frameController: {
+			isFrameSelectionActive: () => true,
+			clearFrameSelection: () => calls.push("clear-frame"),
+			handleFrameDragMove: (...args) => calls.push(["frame-move", ...args]),
+			handleFrameDragEnd: (...args) => calls.push(["frame-end", ...args]),
+			handleFrameResizeMove: (...args) =>
+				calls.push(["frame-resize-move", ...args]),
+			handleFrameResizeEnd: (...args) =>
+				calls.push(["frame-resize-end", ...args]),
+			handleFrameRotateMove: (...args) =>
+				calls.push(["frame-rotate-move", ...args]),
+			handleFrameRotateEnd: (...args) =>
+				calls.push(["frame-rotate-end", ...args]),
+			handleFrameAnchorDragMove: (...args) =>
+				calls.push(["frame-anchor-move", ...args]),
+			handleFrameAnchorDragEnd: (...args) =>
+				calls.push(["frame-anchor-end", ...args]),
+		},
+		viewportToolController: {
+			handleViewportTransformDragMove: (...args) =>
+				calls.push(["transform-move", ...args]),
+			handleViewportTransformDragEnd: (...args) =>
+				calls.push(["transform-end", ...args]),
+			pickViewportAssetAtPointer: (...args) => ({ args }),
+			syncViewportTransformGizmo: () => calls.push("sync-gizmo"),
+		},
+		measurementController: {
+			handleMeasurementPointerDown: () => true,
+			handleMeasurementHoverMove: (...args) =>
+				calls.push(["measure-hover", ...args]),
+			clearSelectedMeasurementPoint: () => true,
+			deleteSelectedMeasurement: () => true,
+			syncMeasurementSceneHelpers: () => calls.push("sync-measure-scene"),
+			syncMeasurementOverlay: () => calls.push("sync-measure-overlay"),
+		},
+		viewportAxisGizmoController: {
+			syncViewportAxisGizmo: () => calls.push("sync-axis-gizmo"),
+		},
+		exportController: { id: "export" },
+		updateDropHint: () => calls.push("drop-hint"),
+		updateUi: () => calls.push("ui"),
+		updateOutputFrameOverlay: () => calls.push("overlay"),
+		setStatus: () => calls.push("status"),
+		startZoomToolDrag: () => calls.push("zoom-start"),
+		toggleMeasurementMode: () => calls.push("toggle-measure"),
+		toggleZoomTool: () => calls.push("toggle-zoom"),
+		toggleViewportSelectMode: () => calls.push("toggle-select"),
+		toggleViewportReferenceImageEditMode: () => calls.push("toggle-reference"),
+		toggleViewportTransformMode: () => calls.push("toggle-transform"),
+		toggleViewportPivotEditMode: () => calls.push("toggle-pivot"),
+		openFiles: () => calls.push("open-files"),
+		clearSceneAssetSelection: () => calls.push("clear-assets"),
+		isInteractiveTextTarget: () => false,
+		executeViewportPieAction: (action) => calls.push(["pie-action", action]),
+		handleZoomToolDragMove: () => calls.push("zoom-move"),
+		handleZoomToolDragEnd: () => calls.push("zoom-end"),
+		handleResize: () => calls.push("resize"),
+		fpsMovement: { id: "fps" },
+		pointerControls: { id: "pointer" },
+		getActiveCamera: () => ({ id: "camera" }),
+		guideOverlay: { id: "guide" },
+		syncGuideOverlayState: () => calls.push("sync-guide"),
+		syncViewportProjection: () => calls.push("sync-viewport"),
+		syncShotProjection: () => calls.push("sync-shot"),
+		applyCameraViewProjection: () => calls.push("apply-camera-view"),
+		updateShotCameraHelpers: () => calls.push("shot-helpers"),
+		getActiveCameraViewCamera: () => ({ id: "camera-view" }),
+		getActiveViewportCamera: () => ({ id: "viewport-camera" }),
+		updateCameraSummary: () => calls.push("camera-summary"),
+		t: (value) => `t:${value}`,
+		formatNumber: (value) => `n:${value}`,
+		frameAllCameras: () => calls.push("frame-all"),
+		placeAllCamerasAtHome: () => calls.push("home-all"),
+		syncControlsToMode: () => calls.push("sync-controls"),
+		setExportStatus: () => calls.push("export-status"),
+		projectPresentationSyncSuspendedRef: {
+			get value() {
+				return suspended;
+			},
+			set value(nextValue) {
+				suspended = nextValue;
+			},
+		},
+	});
+
+	assert.equal(bindings.renderer.id, "renderer");
+	assert.equal(bindings.scene.id, "scene");
+	assert.equal(bindings.importReferenceImageFiles("a", "b"), "a|b");
+	assert.equal(bindings.supportsReferenceImageFile("foo.png"), true);
+	assert.equal(bindings.isProjectDirty(), true);
+	assert.equal(bindings.isPackageDirty(), false);
+	assert.equal(bindings.shouldWarnBeforeUnload(), true);
+	assert.deepEqual(bindings.captureViewportProjectionState(), {
+		mode: "ortho",
+	});
+	assert.equal(
+		bindings.restoreViewportProjectionState({ mode: "ortho" }),
+		true,
+	);
+	assert.equal(bindings.startOrbitAroundHitDrag(), true);
+	assert.equal(bindings.isZoomInteractionMode(), true);
+	assert.equal(bindings.isReferenceImageSelectionActive(), true);
+	assert.equal(bindings.isFrameSelectionActive(), true);
+	assert.equal(bindings.handleMeasurementPointerDown(), true);
+	assert.equal(bindings.clearSelectedMeasurementPoint(), true);
+	assert.equal(bindings.deleteSelectedMeasurement(), true);
+	assert.equal(bindings.getShotCameraRollLock(), true);
+	assert.equal(bindings.beginHistoryTransaction("x"), "begin:x");
+	assert.equal(bindings.commitHistoryTransaction("x"), "commit:x");
+
+	bindings.suspendProjectPresentationSync(true);
+	assert.equal(suspended, true);
+	bindings.setShotCameraRollAngleDegrees(12);
+	assert.deepEqual(calls.at(-1), ["roll-set", 12]);
+}
+
+console.log("✅ CAMERA_FRAMES runtime controller bindings tests passed!");
